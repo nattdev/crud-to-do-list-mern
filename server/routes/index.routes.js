70eb9fc4ee34ddc8c458a6d1a3b81a837170be64
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-04 03:04:33 -0500

    Add server routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,12 +1,12 @@
-import {Router} from "express";
+import { Router } from "express";
 import { pool } from "../db.js";
 
 const router = Router();
 
 router.get("/ping", async (req, res) => {
-    const result = await pool.query("SELECT 1 + 1 as result");
-    console.log(result[0]);
-    res.json("ping");
+    const [rows] = await pool.query("SELECT 1 + 1 as result");
+    console.log(rows[0]);
+    res.json(rows[0]);
 });
 
 export default router;
