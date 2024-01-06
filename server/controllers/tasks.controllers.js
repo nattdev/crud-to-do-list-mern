bb6db92285105e8b3dfc9ecdf64ff59e051c44b6
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-05 21:15:42 -0500

    Implement handling of POST requests

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,3 +1,5 @@
+import { pool } from "../db.js";
+
 export const getTasks = (req, res) => {
     res.send("Obteniendo tareas");
 }
@@ -6,8 +8,16 @@ export const getTask = (req, res) => {
     res.send("Obteniendo tarea");
 }
 
-export const createTask = (req, res) => {
-    res.send("Creando tarea");
+export const createTask = async (req, res) => {
+    console.log(req.body);
+    const {title, description} = req.body;
+    const [result] = await pool.query("INSERT INTO tasks(title, description) VALUES (?, ?)", [
+        title,
+        description
+    ]);
+    res.json({
+        id: result.insertId,
+        title, description});
 }
 
 export const updateTask = (req, res) => {
