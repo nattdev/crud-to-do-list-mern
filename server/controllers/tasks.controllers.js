cf225c6e62bdd1c5b8bbc9fdbc3b74644208b5b3
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-07 23:49:35 -0500

    Implement handling of GET requests

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,12 +1,17 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-    res.send("Obteniendo tareas");
-}
+export const getTasks = async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM tasks ORDER BY dateCreate ASC");
+    res.json(result);
+};
 
-export const getTask = (req, res) => {
-    res.send("Obteniendo tarea");
-}
+export const getTask = async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [req.params.id]);
+    if (result.length == 0) {
+        return res.status(404).json("Message not found");
+    }
+    res.json(result[0]);
+};
 
 export const createTask = async (req, res) => {
     console.log(req.body);
@@ -18,12 +23,12 @@ export const createTask = async (req, res) => {
     res.json({
         id: result.insertId,
         title, description});
-}
+};
 
 export const updateTask = (req, res) => {
     res.send("Actualizando tarea");
-}
+};
 
 export const deleteTask = (req, res) => {
     res.send("Eliminando tarea");
-}
+};
