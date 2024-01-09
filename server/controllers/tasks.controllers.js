3a4ca33a785a9c6a02b4c3fd1c0566799d968252
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-08 22:32:05 -0500

    Implement handling of DEL requests

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -15,20 +15,26 @@ export const getTask = async (req, res) => {
 
 export const createTask = async (req, res) => {
     console.log(req.body);
-    const {title, description} = req.body;
+    const { title, description } = req.body;
     const [result] = await pool.query("INSERT INTO tasks(title, description) VALUES (?, ?)", [
         title,
         description
     ]);
     res.json({
         id: result.insertId,
-        title, description});
+        title, description
+    });
 };
 
 export const updateTask = (req, res) => {
     res.send("Actualizando tarea");
 };
 
-export const deleteTask = (req, res) => {
-    res.send("Eliminando tarea");
+export const deleteTask = async (req, res) => {
+    const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [req.params.id]);
+
+    if (result.affectedRows  == 0) {
+        return res.status(404).json("Task not found");
+    }
+    return res.sendStatus(204);
 };
