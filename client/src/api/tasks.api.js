3c450ea4f8c4621de8148db7ca2ca489c1d81d65
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-02-07 03:26:59 -0500

    Switch to environment variables

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+
 export const deleteTaskRequest = async (id) => {
-    return await axios.delete(`http://localhost:4000/tasks/${id}`);
+    return await axios.delete(`${URL}/tasks/${id}`);
 }
 
 export const getTasksRequest = async () => {
-    return await axios.get('http://localhost:4000/tasks')
+    return await axios.get(`${URL}/tasks`)
 }
 
 export const createTaskRequest = async (task) =>
-    await axios.post('http://localhost:4000/tasks', task);
+    await axios.post(`${URL}/tasks`, task);
 
 export const getTaskRequest = async (id) => {
-    return await axios.get(`http://localhost:4000/tasks/${id}`);
+    return await axios.get(`${URL}/tasks/${id}`);
 }
 
 export const updateTaskRequest = async (id, newFields) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, newFields);
+    return await axios.put(`${URL}/tasks/${id}`, newFields);
 }
 
 export const toggleTaskRequest = async (id, done) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, done);
+    return await axios.put(`${URL}/tasks/${id}`, done);
 }
--- server/db.js
+++ server/db.js
@@ -1,9 +1,12 @@
-import { createPool } from "mysql2/promise"
+import { createPool } from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "AD123min_?",
-    database: "tasksdb"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
