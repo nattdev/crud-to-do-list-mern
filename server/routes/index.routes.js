bb01496ca66cd4d3fca0ab56f6f84d2596ee748f
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-02-10 19:50:40 -0500

    Add error handling with try-catch

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,9 +4,14 @@ import { pool } from "../db.js";
 const router = Router();
 
 router.get("/ping", async (req, res) => {
-    const [rows] = await pool.query("SELECT 1 + 1 as result");
-    console.log(rows[0]);
-    res.json(rows[0]);
+    try {
+        const [rows] = await pool.query("SELECT 1 + 1 as result");
+        console.log(rows[0]);
+        res.json(rows[0]);
+    } catch (error) {
+        console.error(Error);
+    }
+  
 });
 
 export default router;
